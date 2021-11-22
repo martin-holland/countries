01c4ca17865738772b2d7490b59d5229e2648759
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-11-22 19:05:13 +0200

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Header from "./components/Header";
 
 class App extends Component {
+  state = {
+    data: [],
+    countryNames: [],
+  };
+
+  componentDidMount() {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      this.setState({ data: res.data });
+      console.log(this.state.data);
+      this.setState({ countryNames: this.state.data.name });
+    });
+  }
   render() {
-    return <div>App will be here</div>;
+    return (
+      <>
+        <Header />
+        <div className="background"></div>
+        <div className="countryContainer">
+          {this.state.data.map((country) => (
+            <div className="countryname" key={country.name.common}>
+              <h2>{country.name.common} </h2>
+              <p>Capital: {country.capital}</p>
+              <img src={country.flags.png} alt="country flag svg" />
+              <p>Current Population: {country.population} </p>
+            </div>
+          ))}
+        </div>
+      </>
+    );
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header(props) {
+  return <header>Countries</header>;
+}
+
+export default Header;
