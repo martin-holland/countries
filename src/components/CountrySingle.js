765c4a77b521de3542a0e01337169b05af0ddce0
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-11-30 10:59:27 +0200

    setting up for weather data

--- src/components/CountrySingle.js
+++ src/components/CountrySingle.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class CountrySingle extends Component {
+  state = {
+    country: [],
+    weather: [],
+  };
+
+  componentDidMount() {
+    console.log(this.props);
+
+    function getCountry() {
+      return axios.get(
+        `'https://restcountries.com/v2/capital/${this.props.params.name}'`
+      );
+    }
+
+    function getWeather() {
+      return axios.get(
+        `'api.openweathermap.org/data/2.5/weather?q=${this.props.params.name}&appid=${process.env.REACT_APP_OPENWEATHER_KEY}'`
+      );
+    }
+  }
+
   render() {
     return (
       <div className="countrysingle">
@@ -8,6 +30,7 @@ class CountrySingle extends Component {
           <h2>{this.props.params.name}</h2>
           <p>Current Temperature:</p>
           <p>Current Precipitation:</p>
+          <p>Population: {this.props.params.population}</p>
         </div>
       </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function Home(props) {
   return (
-    <div>
+    <div className="home">
       <h1>Countries</h1>
+      <p>Please use the links above to navigate to the Countries page.</p>
     </div>
   );
 }
